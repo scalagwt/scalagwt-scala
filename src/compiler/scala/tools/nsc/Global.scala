9685f0fbe6cc2e42c3eaeb3f7c9193b1c5333a95
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-04-18 16:30:12 +0000

    Initial version of RemoveNonJavaExpressions. It does not yet work.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/branches/jvm-src@14708 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/Global.scala
+++ src/compiler/scala/tools/nsc/Global.scala
@@ -373,7 +373,15 @@ class Global(var settings: Settings, var reporter: Reporter) extends SymbolTable
 
   object typer extends analyzer.Typer(
     analyzer.NoContext.make(EmptyTree, Global.this.definitions.RootClass, newScope))
-
+  
+  private def icodePhases =
+    List(
+      genicode,        // generate portable intermediate code
+      inliner,         // optimization: do inlining
+      closureElimination, // optimization: get rid of uncalled closures
+      deadCode            // optimization: get rid of dead cpde
+    )
+  
   /** The built-in components.  The full list of components, including
    *  plugins, is computed in the Plugins trait.
    */
@@ -397,13 +405,12 @@ class Global(var settings: Settings, var reporter: Reporter) extends SymbolTable
     constructors,    // move field definitions into constructors
     flatten,         // get rid of inner classes
     mixer,           // do mixin composition
-    cleanup,         // some platform-specific cleanups
-
-    genicode,        // generate portable intermediate code
-    inliner,         // optimization: do inlining
-    closureElimination, // optimization: get rid of uncalled closures
-    deadCode,           // optimization: get rid of dead cpde
-    if (forMSIL) genMSIL else genJVM, // generate .class files
+    cleanup          // some platform-specific cleanups
+  ) ::: (
+    if (settings.target.value != "jvm-src") icodePhases else Nil
+  ) ::: List(
+    if (forMSIL) genMSIL else
+    if (settings.target.value == "jvm-src") genJava else genJVM,
     sampleTransform
   )
 
